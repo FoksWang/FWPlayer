3a45e03ca7371dd6d75d422a80447cae4c4cd67e
Hui Wang <foks.wang@gmail.com>
2019-07-03 19:02:32 +0200

    Update Example app.

--- Example/Pods/Target Support Files/Pods-FWPlayer_Example/Pods-FWPlayer_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-FWPlayer_Example/Pods-FWPlayer_Example-frameworks.sh
@@ -161,10 +161,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/FWPlayer/FWPlayer.framework"
+  install_framework "${PODS_ROOT}/../../FWPlayer/Frameworks/FWPlayerCore.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/FWPlayer/FWPlayer.framework"
+  install_framework "${PODS_ROOT}/../../FWPlayer/Frameworks/FWPlayerCore.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
