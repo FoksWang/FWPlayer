a77b016fa71719411b861d4bd9c875b30fc3ad52
Hui Wang <foks.wang@gmail.com>
2019-07-04 11:54:49 +0200

    pod and xcode settings update, for swift 5.

--- Example/Pods/Target Support Files/FWPlayer/FWPlayer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FWPlayer : NSObject
-@end
-@implementation PodsDummy_FWPlayer
-@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import FWPlayer
 
 class Tests: XCTestCase {
     
